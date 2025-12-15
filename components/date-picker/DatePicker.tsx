28ff9df68a6867ed6a590b8268a0650c2cf6aeda
cmdNiels <73907007+cmdNiels@users.noreply.github.com>
2025-12-15 21:17:12 +0100

    Updated datepicker.

--- components/date-picker/DatePicker.tsx
+++ components/date-picker/DatePicker.tsx
@@ -1,30 +1,28 @@
 "use client";
 
 import { IconChevronDown } from "@tabler/icons-react";
-import { type ChangeEvent, useState } from "react";
+import { type ChangeEvent, type ComponentPropsWithoutRef, forwardRef, useState } from "react";
 
 import cn from "@/cn";
-import Button from "@/components/button/Button";
 import Calendar from "@/components/calendar/Calendar";
-import Input from "@/components/input/Input";
-import Label from "@/components/label/Label";
+import InputGroup from "@/components/input-group/InputGroup";
+import InputGroupButton from "@/components/input-group/InputGroupButton";
+import InputGroupInput from "@/components/input-group/InputGroupInput";
 import Popover from "@/components/popover/Popover";
 import PopoverContent from "@/components/popover/PopoverContent";
 import PopoverTrigger from "@/components/popover/PopoverTrigger";
 
-export default function DatePicker({
-	value,
-	onChange,
-	className,
-}: {
-	value: Date | undefined;
-	onChange?(date: Date | undefined): void;
-	className?: string;
-}) {
+const DatePicker = forwardRef<
+	HTMLDivElement,
+	{
+		value: Date | undefined;
+		onChange?(date: Date | undefined): void;
+	} & Omit<ComponentPropsWithoutRef<typeof InputGroup>, "onChange">
+>(({ value, onChange, className, ...props }, ref) => {
 	const [open, setOpen] = useState(false);
 	const [time, setTime] = useState(() => {
 		if (!value) {
-			return "10:30:00";
+			return "00:00:00";
 		}
 		const hours = String(value.getHours()).padStart(2, "0");
 		const minutes = String(value.getMinutes()).padStart(2, "0");
@@ -54,36 +52,30 @@ export default function DatePicker({
 	};
 
 	return (
-		<div className={cn("flex gap-4", className)}>
-			<div className="flex flex-col gap-3">
-				<Label htmlFor="date-picker" className="px-1">
-					Date
-				</Label>
-				<Popover open={open} onOpenChange={setOpen}>
-					<PopoverTrigger>
-						<Button variant="outline" id="date-picker" className="w-32 justify-between font-normal">
-							{value ? value.toLocaleDateString() : "Select date"}
-							<IconChevronDown />
-						</Button>
-					</PopoverTrigger>
-					<PopoverContent className="w-auto overflow-hidden p-0" align="start">
-						<Calendar mode="single" selected={value} captionLayout="dropdown" onSelect={handleDateSelect} />
-					</PopoverContent>
-				</Popover>
-			</div>
-			<div className="flex flex-col gap-3">
-				<Label htmlFor="time-picker" className="px-1">
-					Time
-				</Label>
-				<Input
-					type="time"
-					id="time-picker"
-					step="1"
-					value={time}
-					onChange={handleTimeChange}
-					className="appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
-				/>
-			</div>
-		</div>
+		<InputGroup ref={ref} className={cn("w-fit", className)} {...props}>
+			<Popover open={open} onOpenChange={setOpen}>
+				<PopoverTrigger>
+					<InputGroupButton size="sm" variant="none" className="font-normal">
+						{value ? value.toLocaleDateString() : "Select date"}
+						<IconChevronDown />
+					</InputGroupButton>
+				</PopoverTrigger>
+				<PopoverContent className="w-auto overflow-hidden p-0" align="start">
+					<Calendar mode="single" selected={value} captionLayout="dropdown" onSelect={handleDateSelect} />
+				</PopoverContent>
+			</Popover>
+			<InputGroupInput
+				type="time"
+				id="time-picker"
+				step="1"
+				value={time}
+				onChange={handleTimeChange}
+				className="appearance-none [&::-webkit-calendar-picker-indicator]:hidden [&::-webkit-calendar-picker-indicator]:appearance-none"
+			/>
+		</InputGroup>
 	);
-}
+});
+
+DatePicker.displayName = "DatePicker";
+
+export default DatePicker;
