c5932ff442b51cece90463cdb9d67a2713ab5ab7
cmdNiels <73907007+cmdNiels@users.noreply.github.com>
2026-01-23 17:12:39 +0100

    Updated height on `Table` component

--- components/table/Table.tsx
+++ components/table/Table.tsx
@@ -6,7 +6,7 @@ import type { ComponentProps } from "react";
 
 export default function Table({ className, ...props }: ComponentProps<"table">) {
 	return (
-		<div data-slot="table-container" className="relative w-full overflow-x-auto">
+		<div data-slot="table-container" className="relative size-full overflow-x-auto">
 			<table data-slot="table" className={cn("w-full caption-bottom text-sm", className)} {...props} />
 		</div>
 	);
